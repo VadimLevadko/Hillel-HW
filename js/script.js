7bb1375c5c3e8b6982ca7f15d8ef4ab2559f7a6a
VadimLevadko <vadimlevadko@gmail.com>
2024-07-31 22:04:16 +0300

    Homework 3 finished

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const firstNumber = +prompt('What is the first number?');
+const secondNumber = +prompt('What is the second number?');
+
+console.log(`Результат: ${firstNumber} + ${secondNumber} = ${firstNumber + secondNumber}`);
+console.log(`Результат: ${firstNumber} - ${secondNumber} = ${firstNumber - secondNumber}`);
+console.log(`Результат: ${firstNumber} * ${secondNumber} = ${firstNumber * secondNumber}`);
+console.log(`Результат: ${firstNumber} / ${secondNumber} = ${firstNumber / secondNumber}`);
