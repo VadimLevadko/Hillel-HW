74d4fd68c060853df393bbe3ddea75a8dec0eb80
VadimLevadko <vadimlevadko@gmail.com>
2024-08-06 00:08:30 +0300

    Homework 4 finished

--- js/script.js
+++ js/script.js
@@ -1,1 +1,28 @@
 'use strict';
+
+const name = prompt('What is your name?');
+
+const age = prompt('What is your age?');
+
+const likesProgramming = confirm('Do you like programming?');
+
+console.log(name, age, likesProgramming);
+
+
+console.log(typeof name, typeof age, typeof likesProgramming);
+
+const greeting =  `Welcome ${name}.`;
+
+alert(greeting)
+
+
+const numericAge = Number(age)
+
+const nextAge = numericAge + 1
+
+console.log(nextAge)
+
+
+const isAdult = age >= 18
+
+console.log(isAdult)
