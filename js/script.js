61ce2d1c3922825ffa85a01212abed1d709801a5
VadimLevadko <vadimlevadko@gmail.com>
2024-08-06 23:14:32 +0300

    Homework 8 is finished

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
 'use strict';
+
+let firstNum = +prompt('What is the first number?');
+
+while(firstNum <= 0 || isNaN(firstNum)) {
+    firstNum = +prompt('Error! What is the first number?');
+}
+
+let secondNum = +prompt('What is the second number?');
+
+while(secondNum <= 0 || isNaN(secondNum)) {
+    secondNum = +prompt('Error! What is the second number?');
+}
+
+let thirdNum = +prompt('What is the third number?');
+
+while(thirdNum <= 0 || isNaN(thirdNum)) {
+    thirdNum = +prompt('Error! What is the third number?');
+}
+
+alert(`Arithmetic mean of the numbers: ${firstNum}, ${secondNum} and ${thirdNum} = ${((firstNum + secondNum + thirdNum) / 3).toFixed(1)}`);
