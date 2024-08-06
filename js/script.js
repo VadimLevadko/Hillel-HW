219e31f812de770f8f8104404ef95e85ab50eff7
VadimLevadko <vadimlevadko@gmail.com>
2024-08-06 22:46:46 +0300

    Homework 7 is finished

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+let hours = +prompt('Введіть кількість годин');
+
+while(hours <= 0 || isNaN(hours)) {
+    hours = +prompt('Введить правильну кількість годин!');
+}
+
+const hoursToSeconds = hours * 60 * 60
+
+alert(`В ${hours} год. - ${hoursToSeconds} секунд.`);
