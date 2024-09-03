e91d311c491f060494831628ab94ba12693a6fbf
VadimLevadko <vadimlevadko@gmail.com>
2024-09-03 14:47:34 +0300

    Homework 17 finished

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const array = [1, 2, 3, 4, 5, 6, 7];
+
+const removeElement = function (arr, num) {
+  const result = [];
+
+  for (const i of arr) {
+    if (i !== num) {
+      result.push(i);
+    }
+  }
+
+  return result;
+};
+
+const resultRemoveElement = removeElement(array, 5);
+
+console.log(resultRemoveElement);
