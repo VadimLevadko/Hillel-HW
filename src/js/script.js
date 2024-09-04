e239f04861a325594575ff302c995d74e1896efd
VadimLevadko <vadimlevadko@gmail.com>
2024-09-04 21:45:57 +0300

    Homework 19 finished

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,37 @@
 'use strict';
+
+const mainFunction = function (toExponentiation, toMultiply, toDivision, toModulo) {
+  const firstNum = +prompt('What is the first number?');
+  const secondNum = +prompt('What is the second number?');
+
+  toExponentiation(firstNum, secondNum);
+  toMultiply(firstNum, secondNum);
+  toDivision(firstNum, secondNum);
+  toModulo(firstNum, secondNum);
+};
+
+const exponentiation = function (num1, num2) {
+  const result = num1 ** num2;
+
+  alert(`${num1}^${num2} = ${result}`);
+};
+
+const multiply = function (num1, num2) {
+  const result = num1 * num2;
+
+  alert(`${num1} * ${num2} = ${result}`);
+};
+
+const division = function (num1, num2) {
+  const result = num1 / num2;
+
+  alert(`${num1} / ${num2} = ${result}`);
+};
+
+const modulo = function (num1, num2) {
+  const result = num1 % num2;
+
+  alert(`${num1} % ${num2} = ${result}`);
+};
+
+mainFunction(exponentiation, multiply, division, modulo);
