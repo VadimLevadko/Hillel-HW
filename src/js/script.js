6a27b459982c3bb7c44527f18b126350e27f7e90
VadimLevadko <vadimlevadko@gmail.com>
2024-09-06 14:01:38 +0300

    Homework 21 finished

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const arr = [];
+const getLengthArr = +prompt('What is the length of your array?');
+
+for (let i = 0; i < getLengthArr; i++) {
+  const someValue = +prompt('Enter some value for array (ONLY NUMBERS!)');
+
+  arr.push(someValue);
+}
+console.log(arr);
+
+arr.sort((a, b) => a - b);
+console.log(arr);
+
+arr.splice(2, 3);
+console.log(arr);
