ac0026157882d5ab785f552ccd5f16a7cf745c70
VadimLevadko <vadimlevadko@gmail.com>
2024-09-08 21:51:29 +0300

    Homework 22 change filter to reduce

--- src/js/script.js
+++ src/js/script.js
@@ -2,8 +2,29 @@
 
 const someArr = [16, -37, 54, -4, 72, -56, 47, 4, -16, 25, -37, 46, 4, -51, 27, -63, 4, -54, 76, -4, 12, -35, 4, 47];
 
-const result = {
+const result = someArr.reduce((acc, item) => {
+  if (item > 0) {
+    acc.sum += item;
+    acc.countPositiveNum++;
+
+    if (item % 2 !== 0) {
+      acc.oddNum.count++;
+      acc.pairedNum.sum += item;
+    } else {
+      acc.pairedNum.count++;
+      acc.oddNum.sum += item;
+    }
+
+    acc.productOfPositiveEl *= item;
+
+    return acc;
+  }
+  acc.countNegativeNum++;
+
+  return acc;
+}, {
   sum: 0,
+  countPositiveNum: 0,
   countNegativeNum: 0,
   oddNum: {
     count: 0,
@@ -14,32 +35,9 @@ const result = {
     sum: 0,
   },
   productOfPositiveEl: 1,
-};
-
-const filteredArr = someArr.filter((item) => {
-  if (item > 0) {
-    result.sum += item;
-
-    if (item % 2 !== 0) {
-      result.oddNum.count++;
-      result.pairedNum.sum += item;
-    } else {
-      result.pairedNum.count++;
-      result.oddNum.sum += item;
-    }
-
-    result.productOfPositiveEl *= item;
-
-    return true;
-  }
-  result.countNegativeNum++;
-
-  return false;
 });
 
-console.log(filteredArr);
-
-console.log(`Довжина масиву - ${filteredArr.length}. Сума всіх позитивних чисел - ${result.sum}`);
+console.log(`Кількість позитивних елементів - ${result.countPositiveNum}. Сума всіх позитивних чисел - ${result.sum}`);
 
 const minNumber = Math.min(...someArr);
 console.log(`Мінімальний елемент в масиві: ${minNumber}, його Index - ${someArr.indexOf(minNumber)}`);
