834c854f85734f9ad909d6d4b2632924b70cddd3
vadim <vadimlevadko@gmail.com>
2024-09-24 00:23:27 +0300

    Homework 27 finished

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,25 @@
 'use strict';
+
+const user = {
+  firstName: 'John',
+  lastName: 'Doe',
+};
+
+const getFullName = function () {
+  return `${this.firstName} ${this.lastName}`;
+};
+
+const apply = (func, context, ...args) => {
+  let result = null;
+  context.func = func;
+  result = context.func(...args);
+  delete context.func;
+  return result;
+};
+
+const bind = (func, context, args = []) => function () {
+  return apply(func, context, ...args);
+};
+
+const bindContext = bind(getFullName, user);
+console.log(bindContext());
