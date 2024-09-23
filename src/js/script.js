866def8d2864cc3863df852e3ebb153d32756f65
vadim <vadimlevadko@gmail.com>
2024-09-24 00:33:52 +0300

    Homework 30 finished

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const sumNum = () => {
+  let result = 0;
+
+  return function (num) {
+    result += num;
+    return result;
+  };
+};
+
+const test = sumNum();
+console.log(test(3));
+console.log(test(5));
+console.log(test(20));
