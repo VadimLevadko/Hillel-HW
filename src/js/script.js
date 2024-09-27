325f8e99984c3ab4f3699c2067ce68e29f71d806
vadim <vadimlevadko@gmail.com>
2024-09-27 18:43:08 +0300

    Homework 32 finished

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,28 @@
 'use strict';
+
+const body = document.querySelector('body');
+
+const createTable = (columnCount = 10, rowCount = 10) => {
+  const table = document.createElement('table');
+  table.style.border = '2px solid black';
+  table.style.textAlign = 'center';
+  table.style.width = '500px';
+  table.style.height = '500px';
+  let currentValue = 1;
+
+  for (let i = 0; i < columnCount; i++) {
+    const tableTr = document.createElement('tr');
+    for (let j = 0; j < rowCount; j++) {
+      const td = document.createElement('td');
+      td.innerHTML = currentValue;
+      td.style.border = '1px solid black';
+      currentValue++;
+      tableTr.appendChild(td);
+    }
+    table.appendChild(tableTr);
+  }
+
+  body.appendChild(table);
+};
+
+createTable();
