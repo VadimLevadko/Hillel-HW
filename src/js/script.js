fdc8fe13507a457c8e11677d738fc7ea90b1b9d7
vadim <vadimlevadko@gmail.com>
2024-09-27 18:53:15 +0300

    Homework 33 finished

--- src/js/script.js
+++ src/js/script.js
@@ -3,31 +3,14 @@
 const body = document.querySelector('body');
 const PATH_IMAGE = './images/';
 
-const getRandomUniqueNumber = () => {
-  const usedIndexArr = [];
-
-  return function generateRandomUniqueNumber() {
-    const randomIndex = Math.floor(Math.random() * 9) + 1;
-
-    if (!usedIndexArr.includes(randomIndex)) {
-      usedIndexArr.push(randomIndex);
-      return randomIndex;
-    }
-    return generateRandomUniqueNumber();
-  };
-};
-
 const generateImages = () => {
-  const getIndex = getRandomUniqueNumber();
+  const randomIndex = Math.floor(Math.random() * 9) + 1;
 
-  for (let i = 0; i < 9; i++) {
-    const imageIndex = getIndex();
-    const image = document.createElement('img');
-    image.style.display = 'block';
-    image.style.width = `${100}%`;
-    image.src = `${PATH_IMAGE}${imageIndex}.jpg`;
-    body.appendChild(image);
-  }
+  const image = document.createElement('img');
+  image.style.display = 'block';
+  image.style.width = `${100}%`;
+  image.src = `${PATH_IMAGE}${randomIndex}.jpg`;
+  body.appendChild(image);
 };
 
 generateImages();
