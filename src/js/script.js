a7426a02a65844f2315270a5fec77ed6aa470d26
vadim <vadimlevadko@gmail.com>
2024-10-04 16:22:50 +0300

    style: code changes as instructed

--- src/js/script.js
+++ src/js/script.js
@@ -3,13 +3,30 @@
 const body = document.querySelector('body');
 const PATH_IMAGE = './images/';
 
+const createElement = (element = null) => {
+  if (typeof element !== 'string') return;
+  return document.createElement(element);
+};
+
+const appendElementToHTML = (parentEl = null, elementToAppend = null) => {
+  if (!parentEl || !elementToAppend) return;
+  parentEl.appendChild(elementToAppend);
+};
+
+const generateRandomIndex = (startNum = null, endNum = null) => {
+  if (!Number.isInteger(startNum) || !Number.isInteger(endNum)) return;
+  return Math.floor(Math.random() * endNum) + startNum;
+};
+
 const generateImages = () => {
-  const randomIndex = Math.floor(Math.random() * 9) + 1;
-  const image = document.createElement('img');
+  const index = generateRandomIndex(1, 9);
+
+  const image = createElement('img');
   image.style.display = 'block';
   image.style.width = `${100}%`;
-  image.src = `${PATH_IMAGE}${randomIndex}.jpg`;
-  body.appendChild(image);
+  image.src = `${PATH_IMAGE}${index}.jpg`;
+
+  appendElementToHTML(body, image);
 };
 
 generateImages();
