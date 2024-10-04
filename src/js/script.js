a92e41a1b21ac8ad523413e85819ce4afbe521bd
vadim <vadimlevadko@gmail.com>
2024-10-04 17:11:20 +0300

    Homework 34 finished

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const input = document.querySelector('input');
+const info = document.querySelector('.info');
+
+input.addEventListener('focus', () => {
+  info.style.display = 'block';
+});
+input.addEventListener('blur', () => {
+  info.style.display = 'none';
+});
