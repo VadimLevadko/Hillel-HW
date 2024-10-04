f8a6656bad6fa4eb333dae686ff8d439a0cd0362
vadim <vadimlevadko@gmail.com>
2024-10-04 16:05:52 +0300

    style: code changes as instructed

--- src/js/script.js
+++ src/js/script.js
@@ -2,27 +2,49 @@
 
 const body = document.getElementsByTagName('body')[0];
 
-const createTable = (columnCount = 10, rowCount = 10) => {
-  const table = document.createElement('table');
+const appendElementToHTML = (parentEl = null, elementToAppend = null) => {
+  if (!parentEl || !elementToAppend) return;
+  parentEl.appendChild(elementToAppend);
+};
+
+const createElement = (element = null) => {
+  if (typeof element !== 'string') return;
+  return document.createElement(element);
+};
+
+const generateColumnsAndRows = (table) => {
+  if (!table) return;
+
+  let currentValue = 1;
+
+  return function (columnCount, rowCount) {
+    for (let i = 0; i < columnCount; i++) {
+      const tableTr = createElement('tr');
+      for (let j = 0; j < rowCount; j++) {
+        const td = createElement('td');
+        td.innerHTML = currentValue;
+        td.style.border = '1px solid black';
+        currentValue++;
+        appendElementToHTML(tableTr, td);
+      }
+      appendElementToHTML(table, tableTr);
+    }
+  };
+};
+
+const generateTable = (columnCount = null, rowCount = null) => {
+  if (!Number.isInteger(columnCount) || !Number.isInteger(rowCount)) return;
+
+  const table = createElement('table');
   table.style.border = '2px solid black';
   table.style.textAlign = 'center';
   table.style.width = '500px';
   table.style.height = '500px';
-  let currentValue = 1;
 
-  for (let i = 0; i < columnCount; i++) {
-    const tableTr = document.createElement('tr');
-    for (let j = 0; j < rowCount; j++) {
-      const td = document.createElement('td');
-      td.innerHTML = currentValue;
-      td.style.border = '1px solid black';
-      currentValue++;
-      tableTr.appendChild(td);
-    }
-    table.appendChild(tableTr);
-  }
+  const generateTableContent = generateColumnsAndRows(table);
+  generateTableContent(columnCount, rowCount);
 
-  body.appendChild(table);
+  appendElementToHTML(body, table);
 };
 
-createTable();
+generateTable(10, 10);
