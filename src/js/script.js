010c5c96127e5049287d71fe0987a883f0d61e7c
vadim <vadimlevadko@gmail.com>
2024-10-05 21:45:37 +0300

    refactor: code refactored as instructed

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,29 @@
 'use strict';
 
-const input = document.querySelector('input');
-const info = document.querySelector('.info');
+const input = document.querySelector('[data-info-input]');
+const infoMessage = 'lorem ipsum must be required';
 
-input.addEventListener('focus', () => {
-  info.style.display = 'block';
-});
-input.addEventListener('blur', () => {
-  info.style.display = 'none';
-});
+const appendElement = (parentElement, elementToAppend) => {
+  if (!parentElement && !elementToAppend) return;
+  parentElement.appendChild(elementToAppend);
+};
+
+const createInfoDiv = (event) => {
+  const infoDiv = document.createElement('div');
+  const parentEl = event.target.parentElement;
+  infoDiv.style.width = '200px';
+  infoDiv.style.padding = '20px';
+  infoDiv.style.background = 'gray';
+  infoDiv.innerHTML = infoMessage;
+  infoDiv.setAttribute('data-info-div', '');
+  appendElement(parentEl, infoDiv);
+};
+
+const deleteInfoDiv = (event) => {
+  const parentEl = event.target.parentElement;
+  const infoDiv = parentEl.querySelector('[data-info-div]');
+  infoDiv.remove();
+};
+
+input.addEventListener('focus', createInfoDiv);
+input.addEventListener('blur', deleteInfoDiv);
