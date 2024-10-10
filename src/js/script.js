072d6a78dfb99ac524c1a9485ec7c5e4e59ae0cb
vadim <vadimlevadko@gmail.com>
2024-10-11 00:10:58 +0300

    hw36 finished

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,30 @@
 'use strict';
+
+const appendElement = (parentElement, elementToAppend) => {
+  if (!parentElement || !elementToAppend) throw new Error('one of the arguments was missed, the function takes only 2 arguments');
+  parentElement.append(elementToAppend);
+};
+
+const generateList = (listContent) => {
+  if (!listContent || !Array.isArray(listContent)) throw new Error('ListContent must be an array');
+  if (listContent.length === 0) throw new Error('ListContent must be at least one element');
+
+  const ul = document.createElement('ul');
+
+  listContent.forEach((element) => {
+    const li = document.createElement('li');
+
+    if (Array.isArray(element)) {
+      appendElement(li, generateList(element));
+    } else {
+      li.innerHTML = element;
+    }
+
+    appendElement(ul, li);
+  });
+
+  return ul;
+};
+
+const result = generateList([1, 2, [1.1, 1.2, 1.3], 3]);
+appendElement(document.body, result);
