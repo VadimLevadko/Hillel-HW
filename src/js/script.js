ea934d4e906cf22a94a86042a4729f53e549be5c
vadim <vadimlevadko@gmail.com>
2024-10-25 00:39:54 +0300

    hw39 finished

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,34 @@
 'use strict';
+
+(function () {
+  const user = {
+    data: {
+      a: 1,
+      b: 2,
+      c: 3,
+      d: {
+        a1: 1,
+        b1: 2,
+        c1: 3,
+        d1: {
+          a2: 3,
+          b2: 3,
+          c2: 3,
+        },
+      },
+    },
+  };
+
+  const deepFreeze = (obj) => {
+    if (typeof obj !== 'object' || Array.isArray(obj)) throw new Error('Arguments must be object only');
+
+    Object.freeze(obj);
+
+    for (const key in obj) {
+      if (typeof obj[key] === 'object') deepFreeze(obj[key]);
+    }
+
+    return obj;
+  };
+  deepFreeze(user);
+}());
