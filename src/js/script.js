42b3245baec4e575beb7e13daf5e55b9ef7d1170
vadim <vadimlevadko@gmail.com>
2024-10-28 20:45:45 +0200

    hw41 finished

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,41 @@
 'use strict';
+
+const product = {
+  name: null,
+  price: 0,
+  quantity: 1,
+};
+
+Object.defineProperty(product, 'name', {
+  get() {
+    return this._name;
+  },
+  set(value) {
+    if (typeof value !== 'string' || value.trim().length === 0) throw new Error('Value must be a string or not empty.');
+    this._name = value.trim();
+  },
+  enumerable: true,
+  configurable: true,
+});
+
+Object.defineProperty(product, 'price', {
+  get() {
+    return this._price;
+  },
+  set(value) {
+    if (typeof value !== 'number') throw new Error('Value must be a number.');
+    if (value <= 0) throw new Error('Value must be a positive integer.');
+
+    this._price = value;
+  },
+  enumerable: true,
+  configurable: true,
+});
+
+Object.defineProperty(product, 'totalValue', {
+  get() {
+    return this._price * this.quantity;
+  },
+  enumerable: true,
+  configurable: false,
+});
