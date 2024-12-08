b7db95b577e296a1883adb909cd74b1c2cd70d26
vadim <vadimlevadko@gmail.com>
2024-12-08 19:08:11 +0200

    save: save my homework code

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,47 @@
 'use strict';
+
+// eslint-disable-next-line import/extensions
+import { API_POSTS, MAX_POST_ID, errorMessage } from './variables.js';
+
+(function () {
+  const form = document.querySelector('form');
+  const inputId = document.querySelector('[data-form-input-id]');
+
+  (function initializationErrorTextContent() {
+    const errorWrapper = form.querySelector('[data-id-finder-error]');
+
+    errorWrapper.innerHTML = errorMessage;
+  }());
+
+  const appendElement = (parent, elementToAppend) => parent.appendChild(elementToAppend);
+  const showMessage = (element) => element.classList.add('d-none');
+  const hideMessage = (element) => element.classList.remove('d-none');
+  const replaceClassName = (element, oldClassName, newClassName) => (element.classList.contains(`${oldClassName}`) ? element.classList.replace(`${oldClassName}`, `${newClassName}`) : element.classList.add(`${newClassName}`));
+
+  const formValidation = (inputValue) => {
+    const inputWrapper = inputId.closest('[data-input-id-wrapper]');
+    const errorWrapper = form.querySelector('[data-id-finder-error]');
+    const btnSubmit = form.querySelector('[data-form-submit]');
+
+    if (inputValue < 1 || inputValue > MAX_POST_ID) {
+      btnSubmit.disabled = true;
+      hideMessage(errorWrapper);
+      return replaceClassName(inputWrapper, 'border-success', 'border-danger');
+    }
+
+    replaceClassName(inputWrapper, 'border-danger', 'border-success');
+    showMessage(errorWrapper);
+    btnSubmit.disabled = false;
+  };
+
+  inputId.addEventListener('input', (event) => {
+    const inputValue = +event.target.value;
+    formValidation(inputValue);
+  });
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    console.log('click');
+  });
+}());
---
+++ src/js/variables.js
@@ -0,0 +1,5 @@
+export const API_POSTS = 'https://jsonplaceholder.typicode.com/todos';
+
+export const MAX_POST_ID = 100;
+
+export const errorMessage = `Please enter the correct ID for the post (from 1 to ${MAX_POST_ID})`;
